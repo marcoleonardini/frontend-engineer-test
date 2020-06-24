572f524b0acb4e428418f8e92c4e6dd82a608630
Marco Leonardini <marco_leonardini@hotmail.com>
2020-06-24 19:34:59 -0400

    Added Services Consts

---
+++ lib/src/core/const/services.const.dart
@@ -0,0 +1,6 @@
+class ServicesConst {
+  static final String restaurantsBaseURL = 'tripadvisor1.p.rapidapi.com';
+  static final String restaurantsSearch = '/locations/search';
+  static final String serverKey =
+      '2466a463aemsh10daabd0f3fd13cp1171a4jsn0723378229c1';
+}
