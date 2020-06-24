522e47f9ce784c58195eca5bed47b815c85a1e1f
Marco Leonardini <marco_leonardini@hotmail.com>
2020-06-24 18:48:09 -0400

    Added BottomSheet on History

--- lib/src/ui/pages/history.page.dart
+++ lib/src/ui/pages/history.page.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:frontend_engineer_test/src/core/data/history_model.data.dart';
 import 'package:frontend_engineer_test/src/core/models/history.model.dart';
@@ -21,6 +23,7 @@ class HistoryPage extends StatelessWidget {
                 HistoryModel _history = listHistory[index];
 
                 return ListTile(
+                  onTap: () => _settingModalBottomSheet(context, _history),
                   title: Text(_history.place),
                   subtitle: Row(children: [
                     Text('At:  ${_history.day}'),
@@ -32,5 +35,48 @@ class HistoryPage extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  void _settingModalBottomSheet(context, HistoryModel history) {
+    showModalBottomSheet(
+        context: context,
+        builder: (BuildContext bc) {
+          return Container(
+            child: Column(
+              children: <Widget>[
+                Text(
+                  history.place,
+                  style: Theme.of(context).textTheme.headline4,
+                ),
+                SizedBox(
+                  height: 8.0,
+                ),
+                Text(
+                  '${history.day}',
+                  // style: Theme.of(context).textTheme.headline6,
+                ),
+                Expanded(
+                  child: ListView.builder(
+                      itemCount: history.items.length,
+                      itemBuilder: (context, index) {
+                        Item _item = history.items[index];
+
+                        return ListTile(
+                          title: Row(children: [
+                            Text('${_item.name}'),
+                            Spacer(),
+                            Text('${_item.price}'),
+                          ]),
+                        );
+                      }),
+                ),
+                Text(
+                  'Total ${history.amount}',
+                  style: Theme.of(context).textTheme.headline6,
+                ),
+              ],
+            ),
+          );
+        });
   }
 }
